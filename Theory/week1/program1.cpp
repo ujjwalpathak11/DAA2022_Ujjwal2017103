71f8ada6644ae42a18c446d17e8a8a0a090d588b
ujjwalpathak11 <95835615+ujjwalpathak11@users.noreply.github.com>
2022-03-21 22:07:44 +0530

    program1

---
+++ Theory/week1/program1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int countoccurance(int arr[],int n,int x)
+{
+    int frq=0;
+    for(int i=0;i<n;i++)
+     if(x==arr[i])
+       frq++;
+     return frq;
+}
+int main()
+{
+    int arr[] = {1,2,3,4,4,4,4,7,8,8};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int x=4;
+    cout<<countoccurance(arr,n,x);
+    return 0;
+}
