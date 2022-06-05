99e57b2c779ff114d543ef822e04f61437cefce3
ujjwalpathak11 <95835615+ujjwalpathak11@users.noreply.github.com>
2022-06-05 10:28:20 +0530

    Update program3.cpp

--- Theory/week7/program3.cpp
+++ Theory/week7/program3.cpp
@@ -1,80 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct node {
-    int u;
-    int v;
-    int wt; 
-    node(int first, int second, int weight) {
-        u = first;
-        v = second;
-        wt = weight;
-    }
+#define V 4
+#define INF INT_MAX
+int shortestPath(int graph[][V], int u, int v, int k)
+{
+if (k == 0 && u == v)			 
+	return 0;
+if (k == 1 && graph[u][v] != INF) return graph[u][v];
+if (k <= 0)					 
+	return INF;
+int res = INF;
+for (int i = 0; i < V; i++)
+{
+	if (graph[u][i] != INF && u != i && v != i)
+	{
+		int rec_res = shortestPath(graph, i, v, k-1);
+		if (rec_res != INF)
+			res = min(res, graph[u][i] + rec_res);
+	}
+}
+return res;
+}
+int main()
+{
+	int graph[V][V] = { {0, 11, 3, 2},
+			  {INF, 0, INF, 1},
+			  {INF, INF, 0, 6},
+			  {INF, INF, INF, 0}
 };
-
-bool comp(node a, node b) {
-    return a.wt > b.wt; 
-}
-
-int findPar(int u, vector<int> &parent) {
-    if(u== parent[u]) 
-	   return u; 
-    return parent[u] = findPar(parent[u], parent); 
-}
-
-void unionn(int u, int v, vector<int> &parent, vector<int> &rank) {
-    u = findPar(u, parent);
-    v = findPar(v, parent);
-    if(rank[u] < rank[v]) {
-    	parent[u] = v;
-    }
-    else if(rank[v] < rank[u]) {
-    	parent[v] = u; 
-    }
-    else {
-    	parent[v] = u;
-    	rank[u]++; 
-    }
-}
-int main(){
-	int N,m;
-	cin>>N>>m;
-	vector<pair<int,int>> adj[N];   //undirected graph
-	cout<<"enter the edge and its weight"<<endl;
-	for (int i=0;i<m;i++)
-	{
-		int a,b,wt;
-		cin>>a>>b>>wt;
-		adj[a].push_back({b,wt});    //pushing only one edge because we only need one edge to sort rather it became duplicates while sorting later
-	}
-	vector<node> edges; 
-	
-	//converting adjacency list to the easy edge structure
-    for (int i=0;i<N;i++)
-	{
-		for (auto it:adj[i])
-		{
-			edges.push_back(node(i,it.first,it.second));
-		}
-	}
-	sort(edges.begin(), edges.end(), comp); 
-	
-	vector<int> parent(N);
-	for(int i = 0;i<N;i++) 
-	    parent[i] = i; 
-	vector<int> rank(N, 0); 
-	
-	int cost = 0;
-	vector<pair<int,int>> mst; 
-
-	for(auto it : edges) {
-	    if(findPar(it.v, parent) != findPar(it.u, parent))
-        {
-	        cost += it.wt; 
-	        mst.push_back({it.u, it.v}); 
-	        unionn(it.u, it.v, parent, rank); 
-	    }
-	}
-
-	cout << cost << endl;
-	for(auto it : mst) cout << it.first << " - " << it.second << endl;
+	int u = 0, v = 4, k = 2;
+	cout << "Weight of the shortest path is " <<shortestPath(graph, u, v, k);
+	return 0;
 }
